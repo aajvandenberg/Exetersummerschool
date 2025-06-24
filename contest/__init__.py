3deeab70050cc457b8a2d417d9893777ffcd87db
aavandenberg <aavandenberg@student.unimelb.edu.au>
2025-06-25 00:53:46 +1000

    intermediate commit

--- contest/__init__.py
+++ contest/__init__.py
@@ -1,11 +1,4 @@
-from otree.api import (
-    BaseConstants,
-    BaseGroup,
-    BasePlayer,
-    BaseSubsession,
-    Page,
-)
-from otree.views import WaitPage
+from otree.api import *
 
 doc = """
 A splash-screen welcome page for the experiment
@@ -16,23 +9,51 @@ class C(BaseConstants):
     NAME_IN_URL = "contest"
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
+    ENDOWMENT = Currency(10)
+    COST_PER_TICKET = Currency(5)
+    PRICE = Currency(8)
 
 
-class Subsession(BaseSubsession):
-    pass
+class Subsession(BaseSubsession): #This is where I define variables on the subsession (i.e. rounds) level
+    is_paid = models.BooleanField()
+
+    def setup_round(self):
+        self.is_paid = True
+        for group in self.get_groups():
+            group.setup_round()
 
 
-class Group(BaseGroup):
-    pass
+class Group(BaseGroup): #This is where I define variables on the group level
+    prize = models.CurrencyField() #Stores the value of the prize of the lottery
+
+    def setup_round(self):
+        self.prize = C.PRICE
+        for player in self.get_players():
+            player.setup_round()
 
 
-class Player(BasePlayer):
-    pass
+class Player(BasePlayer): #This is where I define variables on the player (i.e. individual) level
+    endowment = models.CurrencyField()
+    cost_per_ticket = models.CurrencyField()
+    tickets_purchased = models.IntegerField() #The reason why I use models.IntegerField() and not tickets_purchased: int is because it links the input to the data or something like that.
+
+    def setup_round(self):
+        self.endowment = C.ENDOWMENT
+        self.cost_per_ticket = C.COST_PER_TICKET
 
 
-# PAGES
+
+
+# def creating_session(subsession): #this function is called at the start of every round (and at the moment of creating the session) and is an alternative to making a waitpage as in SetupRound
+#     subsession.setup_round()
+
+
 class SetupRound(WaitPage):
-    pass
+    wait_for_all_groups = True
+
+    @staticmethod
+    def after_all_players_arrive(subsession): #this function gets called once all players arrived at the waitpage
+        subsession.setup_round()
 
 
 class Intro(Page):
@@ -56,10 +77,10 @@ class EndBlock(Page):
 
 
 page_sequence = [
-    #SetupRound,
+    SetupRound,
     Intro,
     Decision,
-    #DecisionWaitPage,
+    DecisionWaitPage,
     Results,
     EndBlock,
 ]
